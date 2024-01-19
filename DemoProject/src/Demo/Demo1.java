4d84d1a28b97d0074c447c0610ac57639905cec1
durgaprasad <chinna1996.cp@gmail.com>
2024-01-19 17:09:17 +0530

    second message

--- DemoProject/src/Demo/Demo1.java
+++ DemoProject/src/Demo/Demo1.java
@@ -10,6 +10,11 @@ public class Demo1 {
 		System.out.print("doing");
 		System.out.print("doing good ");
 		System.out.print("wt about you ");
+		
+		System.out.print("hii ");
+		System.out.print("i am ");
+
+		System.out.print("doing");
 	}
 
 }
